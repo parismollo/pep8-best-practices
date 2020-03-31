41b8560d2ed3bb36b7a1d5d912dbba3ef3ac43ac
Paris <parismollochristondis@gmail.com>
2020-03-31 20:59:11 +0200

    adicionando tipagens

--- fila_base.py
+++ fila_base.py
@@ -1,15 +1,18 @@
 import abc
+from typing import List
+
+from constantes import TAMANHO_PADRAO_MAX, TAMANHO_PADRAO_MIN
 
 
 class FilaBase(metaclass=abc.ABCMeta):
     codigo: int = 0
-    fila = []
-    clientes_atendidos = []
+    fila: List[str] = []
+    clientes_atendidos: List[str] = []
     senha_atual: str = ''
 
     def reseta_fila(self) -> None:
-        if self.codigo >= 200:
-            self.codigo = 0
+        if self.codigo >= TAMANHO_PADRAO_MAX:
+            self.codigo = TAMANHO_PADRAO_MIN
         else:
             self.codigo += 1
 
--- main.py
+++ main.py
@@ -1,12 +1,21 @@
-from fila_prioritaria import FilaPrioritaria
+# from fila_prioritaria import FilaPrioritaria
 
-fila_teste = FilaPrioritaria()
-fila_teste.atualiza_fila()
-fila_teste.atualiza_fila()
-fila_teste.atualiza_fila()
+from fabrica_fila import FabricaFila
 
-print(fila_teste.chama_cliente(10))
-print(fila_teste.chama_cliente(2))
-print(fila_teste.chama_cliente(4))
+# fila_teste = FilaPrioritaria()
+# fila_teste.atualiza_fila()
+# fila_teste.atualiza_fila()
+# fila_teste.atualiza_fila()
 
-print(fila_teste.estatistica('10/01/1993', 210, 'detail'))
+# print(fila_teste.chama_cliente(10))
+# print(fila_teste.chama_cliente(2))
+# print(fila_teste.chama_cliente(4))
+
+# print(fila_teste.estatistica('10/01/1993', 210, 'detail'))
+
+teste_fabrica = FabricaFila.pega_fila('normal')
+teste_fabrica.atualiza_fila()
+teste_fabrica.atualiza_fila()
+teste_fabrica.atualiza_fila()
+
+print(teste_fabrica.chama_cliente(2))

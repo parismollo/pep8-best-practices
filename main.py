d099763e29b5eb5560e05ced5261d34f5d2f0b16
Paris <parismollochristondis@gmail.com>
2020-03-31 11:04:40 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+from fila_prioritaria import FilaPrioriatia
+
+fila_teste = FilaPrioriatia()
+fila_teste.atualiza_fila()
+fila_teste.atualiza_fila()
+fila_teste.atualiza_fila()
+
+print(fila_teste.chama_cliente(10))
+print(fila_teste.chama_cliente(2))
+print(fila_teste.chama_cliente(4))
+
+print(fila_teste.estatistica('10/01/1993', '198', 'detail'))

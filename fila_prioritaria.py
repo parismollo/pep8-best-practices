8b7d14c837e7cd16e640ef805df3ad016385fd45
Paris <parismollochristondis@gmail.com>
2020-04-01 16:48:38 +0200

    criando injecao de dependencia

--- fila_prioritaria.py
+++ fila_prioritaria.py
@@ -1,7 +1,11 @@
-from typing import Dict, Union, List
+from typing import Union
 
 from fila_base import FilaBase
 from constantes import CODIGO_PRIORITARIO
+from estatistica_resumida import EstatisticaResumida
+from estatistica_detalhada import EstatisticaDetalhada
+
+Classes = Union[EstatisticaResumida, EstatisticaDetalhada]
 
 
 class FilaPrioritaria(FilaBase):
@@ -13,16 +17,6 @@ class FilaPrioritaria(FilaBase):
         self.clientes_atendidos.append(cliente_atual)
         return f'Cliente atual: {cliente_atual}, dirija-se ao caixa {caixa}'
 
-    def estatistica(self, dia: str, agencia: int, flag: str) -> dict:
-        estatistica: Dict[str, Union[List[str], str, int]] = {}
-        if flag != 'detail':
-            estatistica[f'{agencia}-{dia}'] = len(self.clientes_atendidos)
-        else:
-            estatistica['dia'] = dia
-            estatistica['agencia'] = agencia
-            estatistica['clientes_atendidos'] = self.clientes_atendidos
-            estatistica['quantidade_clientes_atendidos'] = (
-                len(self.clientes_atendidos)
-            )
+    def estatistica(self, retorna_estatistica: Classes) -> dict:
 
-        return estatistica
+        return retorna_estatistica.roda_estatistica(self.clientes_atendidos)
--- main.py
+++ main.py
@@ -1,21 +1,14 @@
-# from fila_prioritaria import FilaPrioritaria
+
 
 from fabrica_fila import FabricaFila
+# from estatistica_detalhada import EstatisticaDetalhada
+from estatistica_resumida import EstatisticaResumida
 
-# fila_teste = FilaPrioritaria()
-# fila_teste.atualiza_fila()
-# fila_teste.atualiza_fila()
-# fila_teste.atualiza_fila()
 
-# print(fila_teste.chama_cliente(10))
-# print(fila_teste.chama_cliente(2))
-# print(fila_teste.chama_cliente(4))
-
-# print(fila_teste.estatistica('10/01/1993', 210, 'detail'))
-
-teste_fabrica = FabricaFila.pega_fila('normal')
+teste_fabrica = FabricaFila.pega_fila('prioritaria')
 teste_fabrica.atualiza_fila()
 teste_fabrica.atualiza_fila()
 teste_fabrica.atualiza_fila()
 
 print(teste_fabrica.chama_cliente(2))
+print(teste_fabrica.estatistica(EstatisticaResumida('17/02/1999', 10)))
